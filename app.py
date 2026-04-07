abd9d7d103d7d7d716b57f0575cd66971b468ff4
Kartik Baliyan <kartikbaliyan00004@gmail.com>
2026-04-07 23:40:12 +0530

    text extraction initialised

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+import pytesseract
+from pdf2image import convert_from_bytes
 
-st.title("📄 PDF Text Extractor")
+# Set path (important for Windows)
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-# Upload PDF
+st.title(" Smart PDF Reader (Text + OCR)")
+
 uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
 
 if uploaded_file is not None:
-    # Read PDF
+    
+   #for normal pdf
     reader = PdfReader(uploaded_file)
-
     text = ""
 
-    # Extract text from each page
     for page in reader.pages:
-        text += page.extract_text()
+        extracted = page.extract_text()
+        if extracted:
+            text += extracted
 
-    # Show extracted text
-    st.subheader("📜 Extracted Text:")
-    st.write(text[:2000])  # show first 2000 characters
+    # for scanned pdf
+    if len(text.strip()) < 50:
+        st.warning(" Using OCR (scanned PDF detected)")
+        
+        uploaded_file.seek(0)  
+        pdf_bytes = uploaded_file.read()
+
+        images = convert_from_bytes(pdf_bytes)
+        text = ""
+
+        for img in images:
+            text += pytesseract.image_to_string(img)
+
+    #  result
+    st.subheader(" Extracted Text:")
+    st.write(text[:2000])
