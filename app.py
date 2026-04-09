ce9b32334c0f7d4f0f3dfa3ab257f26f8eedde99
Kartik Baliyan <kartikbaliyan00004@gmail.com>
2026-04-09 23:00:59 +0530

    enhanced for multiple file types

--- app.py
+++ app.py
@@ -1,39 +1,18 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-import pytesseract
-from pdf2image import convert_from_bytes
+from content import load_file
 
+st.title(" Smart Document Reader (PDF + OCR + Multi-format)")
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-st.title(" Smart PDF Reader (Text + OCR)")
-
-uploaded_file = st.file_uploader("Upload your PDF", type="pdf")
+uploaded_file = st.file_uploader(
+    "Upload your file",
+    type=["pdf", "txt", "md", "xls", "xlsx"]
+)
 
 if uploaded_file is not None:
-    
-   #for normal pdf
-    reader = PdfReader(uploaded_file)
-    text = ""
 
-    for page in reader.pages:
-        extracted = page.extract_text()
-        if extracted:
-            text += extracted
+    text = load_file(uploaded_file)
 
-    # for scanned pdf
-    if len(text.strip()) < 50:
-        st.warning(" Using OCR (scanned PDF detected)")
-        
-        uploaded_file.seek(0)  
-        pdf_bytes = uploaded_file.read()
-
-        images = convert_from_bytes(pdf_bytes)
-        text = ""
-
-        for img in images:
-            text += pytesseract.image_to_string(img)
-
-    #  result
     st.subheader(" Extracted Text:")
     st.write(text[:2000])
+
+    st.write(f"Total characters: {len(text)}")
