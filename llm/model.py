d6ad4fd7459b3b33e9dbbed2b4b3cd3417e61eef
Kartik Baliyan <kartikbaliyan00004@gmail.com>
2026-04-14 00:29:32 +0530

    api key setup

--- llm/model.py
+++ llm/model.py
@@ -1,9 +1,16 @@
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
-#API
-genai.configure(api_key="AIzaSyDzecEHxd0WPDsTBNejTB4v85kT4O5ZE54")
 
-model = genai.GenerativeModel("gemini-2.5-flash-lite")
+load_dotenv()
+
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def ask_llm(prompt):
     response = model.generate_content(prompt)
